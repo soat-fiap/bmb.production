1d4c1ebdab137230947e549b38e0e64f10691b64
italo <italo.pessoa@hotmail.com>
2024-10-26 21:02:30 -0300

    test: update unit tests

--- src/Bmb.Production.Core/Contracts/IKitchenOrderRepository.cs
+++ src/Bmb.Production.Core/Contracts/IKitchenOrderRepository.cs
@@ -2,7 +2,7 @@ using Bmb.Production.Core.Model.Dto;
 
 namespace Bmb.Production.Core.Contracts;
 
-public interface IKitchenQueueGateway
+public interface IKitchenOrderRepository
 {
     Task SaveAsync(KitchenOrderDto order, CancellationToken cancellationToken = default);
 }
