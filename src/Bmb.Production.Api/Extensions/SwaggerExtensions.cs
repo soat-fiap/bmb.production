c65f0840a9db0baaedae875b1a4cadf76157e002
italo <italo.pessoa@hotmail.com>
2024-11-20 14:58:10 -0300

    fix: add semantic-release version to open api file

--- src/Bmb.Production.Api/Extensions/SwaggerExtensions.cs
+++ src/Bmb.Production.Api/Extensions/SwaggerExtensions.cs
@@ -10,11 +10,12 @@ internal static class SwaggerExtensions
 {
     internal static void SetupSwagger(this IServiceCollection services)
     {
+        var version = Assembly.GetExecutingAssembly().GetName().Version;
         services.AddSwaggerGen(c =>
         {
-            c.SwaggerDoc("v1", new OpenApiInfo
+            c.SwaggerDoc($"v{version.Major}", new OpenApiInfo
             {
-                Title = "Kitchen Line API", Version = "v1", Extensions =
+                Title = "Kitchen Line API", Version = $"v{version.Major}.{version.Minor}.{version.Build}", Extensions =
                 {
                     {
                         "x-logo",
